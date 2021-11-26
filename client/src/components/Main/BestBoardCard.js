a765b65e31534b1ef8e1b69cbc96b46b936f5d95
Soujiro-a <qmaecx@naver.com>
2021-11-26 17:28:12 +0900

    [Client] 메인페이지 구현, [Server] 메인페이지에 따른 라우터 추가

---
+++ client/src/components/Main/BestBoardCard.js
@@ -0,0 +1,59 @@
+import Link from "next/link";
+import styles from "../../../styles/components/Main/BoardCard.module.css";
+import { MdThumbUp } from "react-icons/md";
+import { FaRegComment } from "react-icons/fa";
+import classNames from "classnames";
+import axios from "axios";
+
+export default function BestBoardCard({ articleList, boardList }) {
+  return (
+    <div
+      className={classNames({
+        [styles["board-card"]]: true,
+        [styles["best"]]: true,
+      })}
+    >
+      <div className={styles["head"]}>
+        <div className={styles["title-side"]}>
+          <div className={styles["board-icon"]}></div>
+          <h2>토픽 베스트</h2>
+        </div>
+        <Link
+          href={{
+            pathname: "/topics/[slug]",
+            query: { slug: "토픽-베스트" },
+          }}
+          passHref
+        >
+          <a>더보기 {`>`}</a>
+        </Link>
+      </div>
+      <div className={styles["body"]}>
+        <ul className={styles["article-list"]}>
+          {articleList.map((article) => {
+            return (
+              <li key={article._id}>
+                <div className={styles["article-title"]}>
+                  <span className={styles["board-tag"]}>
+                    {boardList[article.board]}
+                  </span>
+                  {article.title}
+                </div>
+                <div className={styles["count-display"]}>
+                  <div className={styles["count-item"]}>
+                    <MdThumbUp className={styles["icon"]} />
+                    <span>{article.thumbupCount}</span>
+                  </div>
+                  <div className={styles["count-item"]}>
+                    <FaRegComment className={styles["icon"]} />
+                    <span>{article.commentCount}</span>
+                  </div>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+}
---
+++ client/src/components/Main/BoardCard.js
@@ -0,0 +1,43 @@
+import Link from "next/link";
+import styles from "../../../styles/components/Main/BoardCard.module.css";
+import { MdOutlineRemoveRedEye } from "react-icons/md";
+import { BsEye } from "react-icons/bs";
+
+export default function BoardCard({ title, slug, articleList }) {
+  return (
+    <div className={styles["board-card"]}>
+      <div className={styles["head"]}>
+        <div className={styles["title-side"]}>
+          <div className={styles["board-icon"]}></div>
+          <h2>{title}</h2>
+        </div>
+        <Link
+          href={{
+            pathname: "/topics/[slug]",
+            query: { slug },
+          }}
+          passHref
+        >
+          <a>더보기 {`>`}</a>
+        </Link>
+      </div>
+      <div className={styles["body"]}>
+        <ul className={styles["article-list"]}>
+          {articleList.map((article) => {
+            return (
+              <li key={article._id}>
+                <span className={styles["article-title"]}>{article.title}</span>
+                <div className={styles["count-display"]}>
+                  <div className={styles["count-item"]}>
+                    <BsEye className={styles["icon"]} />
+                    {article.viewCount}
+                  </div>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+}
